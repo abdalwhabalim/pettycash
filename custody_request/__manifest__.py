cdac912becb86c42b81b858370ee5c17d56b8a20
Abdelwhab Babikr <abdalwhab.babeker@yahoo.com>
2023-03-18 16:45:30 +0400

    Update __manifest__.py

--- custody_request/__manifest__.py
+++ custody_request/__manifest__.py
@@ -10,8 +10,6 @@
     'support': 'support@softguidetech.com',
     'website': 'https://softguidetech.com',
     'license': 'OPL-1',
-    'price': '55',
-    'currency': 'EUR',
     'data': [
 
         'security/security_view.xml',
